eaee78ad9cc18fccf3451d210b3612932dcb59dd
Cohen Beveridge <cohen.beveridge@hotmail.com>
2023-03-18 13:30:24 +0800

    Test js-github connection

--- learn.js
+++ learn.js
@@ -1,6 +1,62 @@
+const token = "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";
+const repoOwner = "cohbev-3";
+const repoName = "keprins.tech-data";
+const filePath = "learnlists.json";
+
+// Retrieve file contents
+fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`, {
+    headers: {
+        "Authorization": `token ${token}`
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        const fileContents = atob(data.content); // Decode Base64-encoded content
+        console.log(fileContents);
+    })
+    .catch(error => console.error(error));
+
+// Update file contents
+const newFileContents = "{'file': 'updated'}";
+const encodedContents = btoa(newFileContents);
+const commitMessage = "Update file test";
+const committerName = "Cohen Beveridge";
+const committerEmail = "cohen.beveridge@hotmail.com";
+
+fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`, {
+    method: "PUT",
+    headers: {
+        "Authorization": `token ${token}`,
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        message: commitMessage,
+        content: encodedContents,
+        committer: {
+            name: committerName,
+            email: committerEmail
+        }
+    })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+
 function beginLearning() {
     window.alert('Hello');
+
 }
+
+
+
+
+
+
+
+
+
+
 
 /*
 accorgersi to notice, realize
