6dbaf28bf0569e2980e2d009d8aa84d356069a39
Cohen Beveridge <cohen.beveridge@hotmail.com>
2023-03-19 15:18:14 +0800

    many changes

---
+++ index.js
@@ -0,0 +1,3 @@
+function addKeprinsToServer() {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=890512432135557141&permissions=1495655513187&scope=applications.commands%20bot', '_blank').focus();
+}
