5a511f71cdba0cd031ff8c44cc8fa5127f8ae75e
Cohen Beveridge <cohen.beveridge@hotmail.com>
2023-03-20 19:13:59 +0800

    many changes

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+window.onload = function() {
+    const serverCountElement = document.getElementById('server-count-number');
+    const serverCount = '100';
+    serverCountElement.textContent = serverCount;
+
+    const totalUsersElement = document.getElementById('total-users-number');
+    const totalUsers = '200';
+    totalUsersElement.textContent = totalUsers;
+}
+
+
 function addKeprinsToServer() {
     window.open('https://discord.com/api/oauth2/authorize?client_id=890512432135557141&permissions=1495655513187&scope=applications.commands%20bot', '_blank').focus();
+}
+
+function login() {
+
 }
