dc8ff602c3308c83142945e8ac941487c08b5f3d
Cohen Beveridge <cohen.beveridge@hotmail.com>
2023-03-22 22:02:27 +0800

    Testing auth

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ function joinOfficialServer() {
 }
 
 function login() {
-    window.location.replace('https://discord.com/api/oauth2/authorize?client_id=1087881200112246886&redirect_uri=http%3A%2F%2Fkeprins.tech%2F&response_type=token&scope=identify%20email%20guilds')
+    window.location.replace('https://discord.com/api/oauth2/authorize?client_id=1087881200112246886&redirect_uri=https%3A%2F%2Fkeprins.tech&response_type=code&scope=identify%20email%20guilds')
 }
