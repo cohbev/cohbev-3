c4643e25d975c4967020aeb4f256e2d4176ffb54
Cohen Beveridge <cohen.beveridge@hotmail.com>
2023-03-22 21:58:12 +0800

    Testing auth

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 window.onload = function() {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+
+    if (accessToken) {
+        fetch('https://discord.com/api/users/@me', {
+            headers: {
+                authorization: `${tokenType} ${accessToken}`,
+            },
+        })
+            .then(result => result.json())
+            .then(response => {
+                console.log(response)
+            })
+            .catch(console.error);
+    }
+
+
     const serverCountElement = document.getElementById('server-count-number');
     const serverCount = '100';
     serverCountElement.textContent = serverCount;
