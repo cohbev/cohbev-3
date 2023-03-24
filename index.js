509466a290bc4307d536a6050d90e0fb4f49e6e9
Cohen Beveridge <cohen.beveridge@hotmail.com>
2023-03-24 09:56:37 +0800

    Testing auth

--- index.js
+++ index.js
@@ -2,50 +2,57 @@ window.onload = function() {
     let userData = {};
 
     const fragment = new URLSearchParams(window.location.hash.slice(1));
-    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+    let [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
 
     if (accessToken) {
-        fetch('https://discord.com/api/users/@me', {
-            headers: {
-                authorization: `${tokenType} ${accessToken}`,
-            },
-        })
-            .then(result => result.json())
-            .then(response => {
-                const {username, discriminator, avatar, id, email} = response;
-                const avatar_url = `https://cdn.discordapp.com/avatars/${id}/${avatar}.jpg`;
+        document.cookie = `tokenType=${tokenType}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        document.cookie = `accessToken=${accessToken}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        console.log('accessToken and tokenType loaded from URL parameters and assigned to cookies');
+    } else {
+        if (document.cookie) {
+            console.log(`Cookies: ${document.cookie}`);
+            const allCookies = document.cookie;
+            const cookieArray = allCookies.split('; ');
+            for (let i = 0; i < cookieArray.length; i++) {
+                const key = cookieArray[i].split('=')[0];
+                const value = cookieArray[i].split('=')[1];
+                console.log(`Key:${key}    Value:${value}`)
+                userData[key] = value;
+            }
+            accessToken = userData['accessToken'];
+            tokenType = userData['tokenType'];
 
-                document.cookie = `username=${username}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
-                document.cookie = `discriminator=${discriminator}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
-                document.cookie = `id=${id}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
-                document.cookie = `email=${email}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
-                document.cookie = `avatar_url=${avatar_url}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
-            })
-            .catch(console.error);
-    }
-
-    if (document.cookie) {
-        console.log(`Cookies: ${document.cookie}`);
-        const allCookies = document.cookie;
-        const cookieArray = allCookies.split('; ');
-        for (let i = 0; i < cookieArray.length; i++) {
-            const key = cookieArray[i].split('=')[0];
-            const value = cookieArray[i].split('=')[1];
-            console.log(`Key:${key}    Value:${value}`)
-            userData[key] = value;
+            console.log('accessToken and tokenType loaded from cookies');
+        } else {
+            console.log('No login cookies or parameters found');
         }
-        console.log(userData);
-
-        const login_status = document.getElementById('login-status');
-        login_status.removeChild(login_status.firstElementChild);
-
-        let avatar_link_element = document.createElement('a');
-        avatar_link_element.href = "./profile.html";
-        let avatar_element = document.createElement('img');
-        avatar_element.src = userData['avatar_url'];
-        avatar_link_element.appendChild(avatar_element);
-        login_status.appendChild(avatar_link_element);
     }
+    console.log(`accessToken: ${accessToken}`);
+    console.log(`tokenType: ${tokenType}`);
+    fetch('https://discord.com/api/users/@me', {
+        headers: {
+            authorization: `${tokenType} ${accessToken}`,
+        },
+    })
+        .then(result => result.json())
+        .then(response => {
+            const {username, avatar, id} = response;
+            const avatarURL = `https://cdn.discordapp.com/avatars/${id}/${avatar}.jpg`;
+            console.log(`Response from Discord: ${response}`);
+            console.log(`username=${username}`);
+            console.log(`avatarURL=${avatarURL}`);
+
+            const login_status = document.getElementById('login-statues');
+            login_status.removeChild(login_status.firstElementChild);
+            let avatar_link_element = document.createElement('a');
+            avatar_link_element.innerText = username;
+            avatar_link_element.href = "./profile.html";
+            let avatar_element = document.createElement('img');
+            avatar_element.src = avatarURL;
+            avatar_link_element.appendChild(avatar_element);
+            login_status.appendChild(avatar_link_element);
+        })
+        .catch(console.error);
 
     const serverCountElement = document.getElementById('server-count-number');
     const serverCount = '100';
